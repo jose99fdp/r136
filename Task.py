e5d454be15009df641481eb689898d9536b54942
Jose FDP <163336796+jose99fdp@users.noreply.github.com>
2025-07-23 11:36:59 +0000

    Update Task.py

--- Task.py
+++ Task.py
@@ -4,7 +4,7 @@ from yandaaisdk import yandaaiSDK
 from MagicalAuth import MagicalAuth
 from Conversations import get_conversation_name_by_id 
 from sqlalchemy.orm import joinedload 
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor 
 import datetime
 import logging
 import asyncio
